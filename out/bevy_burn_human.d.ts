2a0a1c8db9d70ab05678568a5b6ccb452b024f85
mosure <mitchell@mosure.me>
2025-12-19 03:17:25 +0000

    Deploying to www from @ mosure/burn_human@a10f63e02a16659b5bbf750cf417530b0c9ac3bc 🚀

--- out/bevy_burn_human.d.ts
+++ out/bevy_burn_human.d.ts
@@ -6,13 +6,13 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly main: (a: number, b: number) => number;
-  readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke___js_sys_2b5c4a24819c7af9___Array_____: (a: number, b: number, c: any) => void;
-  readonly wasm_bindgen_60501b20a8c7db29___closure__destroy___dyn_core_6581d65b4d67ad7b___ops__function__FnMut__js_sys_2b5c4a24819c7af9___Array____Output_______: (a: number, b: number) => void;
-  readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke___js_sys_2b5c4a24819c7af9___Array__web_sys_bff21b01a83441a___features__gen_ResizeObserver__ResizeObserver_____: (a: number, b: number, c: any, d: any) => void;
-  readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke___web_sys_bff21b01a83441a___features__gen_InputEvent__InputEvent_____: (a: number, b: number, c: any) => void;
-  readonly wasm_bindgen_60501b20a8c7db29___closure__destroy___dyn_core_6581d65b4d67ad7b___ops__function__FnMut__web_sys_bff21b01a83441a___features__gen_InputEvent__InputEvent____Output_______: (a: number, b: number) => void;
   readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke___wasm_bindgen_60501b20a8c7db29___JsValue_____: (a: number, b: number, c: any) => void;
   readonly wasm_bindgen_60501b20a8c7db29___closure__destroy___dyn_core_6581d65b4d67ad7b___ops__function__FnMut__wasm_bindgen_60501b20a8c7db29___JsValue____Output_______: (a: number, b: number) => void;
+  readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke___js_sys_2b5c4a24819c7af9___Array__web_sys_bff21b01a83441a___features__gen_ResizeObserver__ResizeObserver_____: (a: number, b: number, c: any, d: any) => void;
+  readonly wasm_bindgen_60501b20a8c7db29___closure__destroy___dyn_core_6581d65b4d67ad7b___ops__function__FnMut__js_sys_2b5c4a24819c7af9___Array____Output_______: (a: number, b: number) => void;
+  readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke___web_sys_bff21b01a83441a___features__gen_InputEvent__InputEvent_____: (a: number, b: number, c: any) => void;
+  readonly wasm_bindgen_60501b20a8c7db29___closure__destroy___dyn_core_6581d65b4d67ad7b___ops__function__FnMut__web_sys_bff21b01a83441a___features__gen_InputEvent__InputEvent____Output_______: (a: number, b: number) => void;
+  readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke___js_sys_2b5c4a24819c7af9___Array_____: (a: number, b: number, c: any) => void;
   readonly wasm_bindgen_60501b20a8c7db29___convert__closures_____invoke______: (a: number, b: number) => void;
   readonly __wbindgen_malloc_command_export: (a: number, b: number) => number;
   readonly __wbindgen_realloc_command_export: (a: number, b: number, c: number, d: number) => number;
